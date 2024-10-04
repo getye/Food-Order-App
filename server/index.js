9de1bc8809b8be05d3d70e0b491ccc4a8479ec13
Getye <getye2008@gmail.com>
2024-10-04 13:21:10 +0300

    v1

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const session = require('express-session');
+const userRouter = require('./routes/userRouter');
+const roleRouter = require('./routes/roleRouter');
+require('dotenv').config();
+const path = require('path');
+const menuRouter = require('./routes/MenuRoute');
+
+const app = express();
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(cors({
+  origin: 'http://localhost:3000', // Change this to your React app's URL
+  credentials: true
+}));
+
+// Set up session middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Set to true if using HTTPS
+}));
+
+app.use('/', userRouter);
+app.use('/', roleRouter);
+app.use('/', menuRouter);
+
+
+const PORT = process.env.PORT || 8001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
