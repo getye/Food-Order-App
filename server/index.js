1a24fcd3ec12507977753f23ec9e7399511f43f4
Getye <getye2008@gmail.com>
2024-10-09 16:04:40 +0300

    v3

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors({
-  origin: 'http://localhost:3000', // Change this to your React app's URL
+  origin: 'http://localhost:3000', 
   credentials: true
 }));
 
